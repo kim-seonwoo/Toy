5dad5826d1cf9e22098dca6dae54c56d6c89e28c
kim-seonwoo <sw204011@gmail.com>
2024-12-23 14:25:52 +0900

    [Set] Design setting

---
+++ Poli/Poli/Global/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Poli
+//
+//  Created by Seonwoo Kim on 12/23/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
